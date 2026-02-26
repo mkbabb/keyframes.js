0359e75ae3d482e573260aca6b012410a9482e6e
Mike Babb <mike7400@gmail.com>
2026-02-26 15:15:34 -0500

    fix: minor UI component tweaks for card, dialog, menubar, popover

--- demo/@/components/ui/button/index.ts
+++ demo/@/components/ui/button/index.ts
@@ -3,7 +3,7 @@ import { type VariantProps, cva } from 'class-variance-authority'
 export { default as Button } from './Button.vue'
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -19,9 +19,9 @@ export const buttonVariants = cva(
       },
       size: {
         default: 'h-10 px-4 py-2',
-        xs: 'h-7 rounded px-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        xs: 'h-7 rounded-lg px-2',
+        sm: 'h-9 rounded-xl px-3',
+        lg: 'h-11 rounded-xl px-8',
         icon: 'h-10 w-10',
       },
     },
